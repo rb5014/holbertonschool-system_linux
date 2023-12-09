a640bb70ba5d20d997c3d0c9d34dfa0683a80b75
Romain Bayle <5014@holbertonstudents.com>
2023-12-09 19:41:24 +0100

    test removing almost all code of long_listin_format printing

--- ls/print_long_listing_format.c
+++ ls/print_long_listing_format.c
@@ -47,16 +47,7 @@ void print_long_listing_format(FileArg *file_array, int nb)
 
 	for (i = 0; i < nb; i++)
 	{
-		if (S_ISDIR(file_array[i].st.st_mode))
-			printf("d");
-		else
-			printf("-");
-		print_permissions(file_array[i].st.st_mode);
-		printf("%li ", file_array[i].st.st_nlink);
-		printf("%s ", getpwuid(file_array[i].st.st_uid)->pw_name);
-		printf("%s ", getgrgid(file_array[i].st.st_gid)->gr_name);
-		printf("%li ", file_array[i].st.st_size);
-		print_time(ctime(&(file_array[i].st.st_mtime)));
+
 		printf("%s\n", file_array[i].name);
 	}
 }
