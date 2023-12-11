689e73448acbd8ff7b5f9ff94a8dd6ea63c1fb66
Romain Bayle <5014@holbertonstudents.com>
2023-12-11 19:24:34 +0100

    works with no memory leaks with 1-main.c

--- getline/_getline.c
+++ getline/_getline.c
@@ -11,12 +11,22 @@ char *_getline(const int fd)
 	int n_bytes_read = 0;
 	char *line = malloc(READ_SIZE);
 
+	if (line == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
 	n_bytes_read = read(fd, line, READ_SIZE);
 
-	if (n_bytes_read == 0)
+	if (n_bytes_read <= 0)
 	{
+		free(line);
 		return (NULL);
 	}
+
+	/* read doesn't put a null-terminating char */
+	line[n_bytes_read] = '\0';
 
 	return (line);
 }
