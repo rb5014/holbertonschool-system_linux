b21d11763443dd7b13bddb593ba2d285a629f634
Bayle Romain <5014@holbertonstudents.com>
2024-02-12 14:33:06 +0100

    memcpy fixed and optimized

--- libasm/5-memcpy.asm
+++ libasm/5-memcpy.asm
@@ -1,4 +1,3 @@
-BITS 64                  ; Set the mode to 64-bit
 
 section .text            ; Start the text section
 	global asm_memcpy    ; Declare the function asm_memcpy as global
@@ -8,18 +7,7 @@ asm_memcpy:              ; Start of the asm_memcpy function
 	;   rdi: pointer to the dest string
 	;   rsi: pointer to the src string
     ;   rdx: number of char to copy
-
-
-mov rcx, 0
-
-.loop:                   ; Start of the loop to iterate through the string
-    cmp rcx, rdx         ; End function when all chars have been copied
-    je .end
-
-    mov dl, [rsi + rcx]  ; Load char in rsi at position rcx
-    mov [rdi + rcx], dl  ; Copy in rdi at position rcx the value in dl
-	inc rcx              ; increment rcx counter
-	jmp .loop            ; Repeat loop
-
-.end:
-    ret
+mov rax, rdi
+mov rcx, rdx
+rep movsb
+ret
