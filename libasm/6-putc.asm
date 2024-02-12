9973f16990294e0ca672508df2904b56f157170d
Bayle Romain <5014@holbertonstudents.com>
2024-02-12 16:44:34 +0100

    asm_putc

---
+++ libasm/6-putc.asm
@@ -0,0 +1,15 @@
+
+section .text            ; Start the text section
+	global asm_putc    ; Declare the function asm_memcpy as global
+
+asm_putc:              ; Start of the asm_memcpy function
+	; Inputs:
+	;   rdi: char to print
+    ; Set up the registers for sys_write
+mov rax, 1  	; Sys_write system call number
+mov [rsi], dil  ; Get char in rdi (dil is lowest byte)
+mov rdi, 1   	; File descriptor for stdout
+mov rdx, 1 		; Length of the message
+syscall 		; Trigger the system call
+ret 			; Return from the function
+
