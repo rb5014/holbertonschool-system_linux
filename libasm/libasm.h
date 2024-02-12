4ab77e543b358d0d4da243fbdda18d728fb5c847
Bayle Romain <5014@holbertonstudents.com>
2024-02-12 16:42:21 +0100

    asm_putc

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -6,4 +6,5 @@ int asm_strncmp(const char *s1, const char *s2, size_t n);
 char *asm_strchr(const char *s, int c);
 char *asm_strstr(const char *haystack, const char *needle);
 void *asm_memcpy(void *dest, const void *src, size_t n);
+size_t asm_putc(int c);
 #endif /* LIBASM */
