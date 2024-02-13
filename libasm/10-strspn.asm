31495124a4f6a1b299cd6d4e2a388eafa4238a86
Bayle Romain <5014@holbertonstudents.com>
2024-02-13 20:12:22 +0100

    asm_strspn done

--- libasm/10-strspn.asm
+++ libasm/10-strspn.asm
@@ -5,33 +5,42 @@ section .text           ; Start the text section
 
 asm_strspn:            ; Start of the asm_strcmp function
 	xor rax, rax         ; Initialize RAX register to 0
-	xor rbx, rbx         ; Initialize RBX register to 0
-	xor rcx, rcx          ; Initialize RCX register to 0
+	xor rbx, rbx         ; Initialize RAX register to 0
+	xor rdx, rdx          ; Initialize RCX register to 0
 
 
 .loop:                  ; Start of the loop
-	cmp rcx, rdx
-	je .end
-	mov al, byte [rdi + rcx]  ; Load byte from the first string into AL
-	mov bl, byte [rsi + rcx]  ; Load byte from the second string into BL
-	inc rcx             ; Increment RCX to move to the next character
 
-	cmp al, 0
-	je .check_end_2
-
+	mov dh, byte [rdi]   ; Load byte from the string into AL
+	test dh, dh
+	jnz .strchr
+	cmp rax, rbx
+	cmovb rax, rbx
+	ret
 
 
-.check_end_2:
-	cmp bl, 0
-	je .end
-	jmp .compare
-.compare:
-		or al, 0x20
-    	or bl, 0x20
+.strchr:
+	xor rcx, rcx          ; Initialize RCX register to 0
+	.strchr_loop:                   ; Start of the loop to iterate through the string
+		mov dl, byte [rsi + rcx]
+    	cmp dh, dl         ; Compare the character with the target character
+    	je .strchr_found            ; If character matches, jump to .found
 
-    	cmp al, bl
-    	je .loop
-.end:
-	sub bl, al
-	mov al, bl
-	ret                 ; Return from the function
+    	test dl, dl          ; Check for end of string (null terminator)
+    	jz .strchr_not_found        ; If end of string, jump to .not_found
+
+    	inc rcx              ; Move to the next character in the string
+    	jmp .strchr_loop            ; Repeat loop
+
+    .strchr_found:                  ; Label for character found
+		inc rbx             ; Increment RCX to move to the next character
+		inc rdi
+    	jmp .loop                   ; Return from the function
+
+    .strchr_not_found:              ; Label for character not found
+    	inc rdi
+		cmp rax, rbx
+		cmovb rax, rbx
+		xor rbx, rbx
+    	jmp .loop
+
