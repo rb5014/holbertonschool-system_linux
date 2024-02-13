1550cb84888f4dfcd8e868dc09be2fbe35d016bc
Bayle Romain <5014@holbertonstudents.com>
2024-02-13 13:36:21 +0100

    asm_strcasecmp fixed again

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -15,39 +15,15 @@ asm_strcasecmp:            ; Start of the asm_strcmp function
 	inc rcx             ; Increment RCX to move to the next character
 
 	cmp al, 0           ; Check if end of the first string is reached
-	je .end_0           ; If so, exit loop and return result
-
-	jmp .check_first_char
-
-
-.check_first_char:
-	;This permits to convert only if it is an uppercase letter
-	cmp al, 'A'
-	jb .check_second_char
-	cmp al, 'Z'
-	ja .check_second_char
-	or al, 0x20
-	jmp .check_second_char
-
-.check_second_char:
-	;This permits to convert only if it is an uppercase letter
-	cmp bl, 'A'
-	jb .compare_chars
-	cmp bl, 'Z'
-	ja .compare_chars
-	or bl, 0x20
-	jmp .compare_chars
-
-.compare_chars:
-	cmp al, bl
-	je .loop
-	jmp .end
-
-.end_0:
-	mov rax, 0
+	jne .compare           	;
 	ret
 
-.end:
-	sub al, bl
-	movsx rax, al
+.compare:
+	or al, 0x20
+	or bl, 0x20
+	cmp al, bl
+	je .loop
+
+	sub bl, al
+	mov al, bl
 	ret                 ; Return from the function
