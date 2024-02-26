1afd7bf27a3e2a7decc83e8c936c884dbd95bc99
Romain BAYLE <5014@holbertonstudents.com>
2024-02-26 11:17:16 +0000

    10-pid_exist.c

--- signals/signals.h
+++ signals/signals.h
@@ -6,4 +6,5 @@ void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
+int pid_exist(pid_t pid);
 #endif /* SIGNALS */
