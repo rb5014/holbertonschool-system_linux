e7648ff62f449265bddca5bf472b5c9112e8e034
Romain BAYLE <5014@holbertonstudents.com>
2024-03-25 12:10:33 +0000

    CPython task 0 && 1

---
+++ 0x08_CPython/1-python.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <Python.h>
+
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t i;
+
+	p = p;
+	printf("[*] Python list info\n");
+	printf("[*] [*] Size of the Python List = %li\n", PyList_Size(p));
+	printf("[*] Allocated = %li\n", list->allocated);
+
+	for (i = 0; i < PyList_Size(p); i++)
+		printf("Element %li: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
