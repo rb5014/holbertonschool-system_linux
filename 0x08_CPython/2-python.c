dc682208eb10413757f3141c2b66ce1197e6cdd7
Romain BAYLE <5014@holbertonstudents.com>
2024-03-28 14:33:11 +0000

    chnage format to z from l for ssize_t printing

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -25,9 +25,9 @@ void print_python_bytes(PyObject *p)
 
 	bytes = bytes;
 	printf("[.] bytes object info\n");
-	printf("  size: %li\n", size);
+	printf("  size: %zi\n", size);
 	printf("  trying string: %s\n", str);
-	printf("  first %li bytes:", size >= 10 ? 10 : size + 1);
+	printf("  first %zi bytes:", size >= 10 ? 10 : size + 1);
 	for (i = 0; (i < size + 1) && (i < 10); i++)
 		printf(" %02hhx", str[i]);
 	printf("\n");
@@ -50,17 +50,17 @@ void print_python_list(PyObject *p)
 
 	if (PyList_CheckExact(p) == 0)
 	{
-		fprintf(stderr, "[ERROR] Invalid List Object\n");
+		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", list->allocated);
+	printf("[*] Size of the Python List = %zi\n", size);
+	printf("[*] Allocated = %zi\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
-		printf("Element %li: %s\n", i, item->ob_type->tp_name);
+		printf("Element %zi: %s\n", i, item->ob_type->tp_name);
 		if (PyBytes_CheckExact(item))
 			print_python_bytes(item);
 	}
