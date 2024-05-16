5a879887cf7127328fe6ed46936d487b0d5c6761
Romain Bayle <5014@holbertonstudents.com>
2024-05-16 16:34:14 +0200

    return nb of chars printed

--- multithreading/1-tprintf.c
+++ multithreading/1-tprintf.c
@@ -3,16 +3,17 @@
 /**
  * tprintf - uses the printf family to print out a given formatted string
  * @format: formatted string to print
- * Return: NULL
+ * Return: number of chars printed
 */
 int tprintf(char const *format, ...)
 {
 	pthread_t id = pthread_self();
 	va_list args;
+	int result;
 
 	printf("[%li] ", id);
 	va_start(args, format);
-	vprintf(format, args);
+	result = vprintf(format, args);
 	va_end(args);
-	pthread_exit(&id);
+	pthread_exit(&result);
 }
