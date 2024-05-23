00f19f54371a54e675c0125060a2a54c73201609
Romain Bayle <5014@holbertonstudents.com>
2024-05-23 18:31:49 +0200

    test 2

--- multithreading/21-prime_factors.c
+++ multithreading/21-prime_factors.c
@@ -1,5 +1,7 @@
-#include "multithreading.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
 /**
 * is_prime - Checks if a number is prime
 * @n: number to check
--- multithreading/list.h
+++ multithreading/list.h
@@ -2,8 +2,7 @@
 #define LIST_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
  * struct node_s - Node structure
  *
