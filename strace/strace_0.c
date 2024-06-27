aed06b6f01bd6705345e546ca826c128bcbfac53
Romain Bayle <5014@holbertonstudents.com>
2024-06-27 18:36:16 +0200

    refactored code

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -2,6 +2,38 @@
 #include "syscalls.h"
 
 /**
+ * trace_syscalls - trace syscalls of child process
+ * @child_pid: child process pid
+*/
+void trace_syscalls(int child_pid)
+{
+	int status, syscall_number, print_next_syscall;
+	struct user_regs_struct user_registers;
+
+	/* Parent process */
+	wait(&status);
+	/* Wait for the child to stop on its first instruction */
+	ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
+	/* Main tracing loop */
+	for (print_next_syscall = 0; !WIFEXITED(status); print_next_syscall ^= 1)
+	{
+		wait(&status);
+		/* Get the registers of the child process */
+		ptrace(PTRACE_GETREGS, child_pid, 0, &user_registers);
+		/* Print the system call number every other time */
+		if (print_next_syscall)
+		{
+			/* Get the system call number */
+			syscall_number = user_registers.orig_rax;
+			printf("%d\n", syscall_number);
+		}
+
+		/* Continue the execution of the child process */
+		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
+	}
+}
+
+/**
 * main - Entry point
 * @argc: The number of arguments
 * @argv: The arguments
@@ -11,8 +43,6 @@
 int main(int argc, char **argv)
 {
 	pid_t child_pid;
-	int status, syscall_number, print_next_syscall;
-	struct user_regs_struct user_registers;
 
 	/* Disable buffering on stdout */
 	setvbuf(stdout, NULL, _IONBF, 0);
@@ -31,28 +61,6 @@ int main(int argc, char **argv)
 		execvp(argv[1], argv + 1);
 	}
 	else
-	{
-		/* Parent process */
-		wait(&status);
-		/* Wait for the child to stop on its first instruction */
-		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-		/* Main tracing loop */
-		for (print_next_syscall = 0; !WIFEXITED(status); print_next_syscall ^= 1)
-		{
-			wait(&status);
-			/* Get the registers of the child process */
-			ptrace(PTRACE_GETREGS, child_pid, 0, &user_registers);
-			/* Print the system call number every other time */
-			if (print_next_syscall)
-			{
-				/* Get the system call number */
-				syscall_number = user_registers.orig_rax;
-				printf("%d\n", syscall_number);
-			}
-
-			/* Continue the execution of the child process */
-			ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-		}
-	}
+		trace_syscalls(child_pid);
 	return (0);
 }
